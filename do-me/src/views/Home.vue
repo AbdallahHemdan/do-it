562188c955697a308fd426f10139047341b03644
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-18 12:44:43 +0200

    adding header and footer

--- do-me/src/views/Home.vue
+++ do-me/src/views/Home.vue
@@ -1,10 +1,49 @@
 <template>
-  <h1>Hello-Home</h1>
+  <div class="home">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todo me</h1>
+        <input
+          class="new-todo"
+          autocomplete="off"
+          placeholder="What needs to be done?"
+          v-model="newTodo"
+          @keyup.enter="addNewTask"
+        />
+      </header>
+    </section>
+    <footer class="info">
+      <h3 class="instruction">Double-click to edit a todo</h3>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      newTodo: "",
+      todos: []
+    };
+  },
+  methods: {
+    addNewTask: function() {
+      if (this.newTodo.trim()) {
+        this.todos.push({
+          title: this.newTodo,
+          completed: false
+        });
+        this.newTodo = "";
+      }
+    }
+  }
+};
 </script>
 
-<style>
+
+<style scoped>
+.instruction {
+  font-size: 20px;
+}
 </style>
